904500477c73b4fc032bc3c0f02d11b6c9ca6b6e
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-06-10 13:15:52 -0400

    initial commit

---
+++ R/draw_poststrat.R
@@ -0,0 +1,68 @@
+#' Get MCMC draws of post-stratified estimate of demog x cd cells
+#'
+#'
+#' @param model stan model from fit_brms
+#' @param poststrat_tgt The poststratification target. It must contain the column
+#' `count`, which is treated as the number of \code{trials} in the binomial model.
+#' @param orig_data original survey data, only used to subset the poststratification,
+#' containing question data.
+#'
+#'
+#' @return A tidy dataset with `qID` x `cd` x `iter` number of rows,
+#' where `qID` is the number of questions (outcomes), `cd` is
+#' the number of geographies, and `iter` is the number of iterations estimated in
+#' the MCMC model.  The demographic cells within a district are averaged across,
+#' and a MRP estimate is computed.
+#' It contains the columns
+#' \describe{
+#' \item{iter}{The number of iterations}
+#' \item{cd}{The geography}
+#' \item{qID}{The question}
+#' \item{p_mrp_est}{The proportion of success, estimated by MRP.}
+#' }
+#'
+#'
+#'
+#' @importFrom dplyr mutate group_by summarize filter rename bind_cols matches
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr pivot_longer
+#' @importFrom readr parse_number
+#' @importFrom brms pp_expect
+#'
+#'
+#' @examples
+poststrat_draws <- function(model, poststrat_tgt, orig_data) {
+
+  # districts (CDs) to loop through
+  cds <- intersect(unique(poststrat_tgt$cd), unique(orig_data$cd))
+
+  # subset to predict on
+  cd_strat <- filter(poststrat_tgt, cd %in% cds) %>%
+    rename(n_response = count)
+
+  # draw, then reshape to tidy form
+  p_draws <- pp_expect(model,
+                       newdata = cd_strat,
+                       allow_new_levels = TRUE,
+                       summary = FALSE)
+
+  # format
+  cds_draws <- p_draws %>%
+    t() %>%
+    as_tibble() %>%
+    mutate(cell = 1:n()) %>%
+    bind_cols(cd_strat, .) %>%
+    pivot_longer(cols = matches("^V"), names_to = "iter", values_to = "pred_n_yes") %>%
+    mutate(iter = parse_number(iter))
+
+
+  # relabel, compute MRP
+  question <- attr(orig_data, "question")
+  cd_est <- cds_draws %>%
+    mutate(qID = question) %>%
+    group_by(qID, cd, iter) %>%
+    summarize(p_mrp = sum(pred_n_yes) / sum(n_response),
+              .groups = "drop")
+
+  cd_est
+}
---
+++ R/fit_brms.R
@@ -0,0 +1,53 @@
+#' Fit a binomial brms model
+#'
+#'
+#' @param .formula model specification
+#' @param .data collapsed survey dataset, built from ccesMRPprep::build_counts
+#' @param .prior prior specification that can be interepreted by brms. The default
+#'   is a standard normal prior, which is tighter than the brms default but has
+#'   shown to have good prior posterior draws
+#' @param prior_PD whether to etimate the prior predictive distribution
+#' @param seed seed for randomization 02138
+#' Fit brms regressions based on input data
+#'
+#' @importFrom brms brm
+#'
+#' @export
+fit_brms <- function(.formula,
+                     .data,
+                     .prior = c(prior_string("normal(0, 1)", class = "b"),
+                                prior_string("normal(0, 1)", class = "sd"),
+                                prior_string("normal(0, 1)", class = "Intercept")),
+                     prior_PD = FALSE,
+                     .seed = 02138) {
+
+  if (prior_PD) {
+    fit <- brm(formula = .formula,
+               data = .data,
+               family = binomial,
+               prior = .prior,
+               sample_prior = "only",
+               cores = 2,
+               chains = 2,
+               iter = 1.0e3,
+               warmup = 0.5e3,
+               seed = .seed)
+  }
+
+  if (!prior_PD) {
+    fit <- brm(formula = .formula,
+               data = .data,
+               family = binomial,
+               prior = .prior,
+               cores = 4,
+               chains = 4,
+               iter = 2e3,
+               control = list(adapt_delta = 0.95,
+                              max_treedepth = 10),
+               seed = .seed)
+  }
+  # attr(fit, "question") <- attr(.data, "question")
+
+  return(fit)
+}
+
