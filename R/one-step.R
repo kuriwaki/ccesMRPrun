a5bf5421ec5eae8c76bb466edb45a9c5ad955b62
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-01-30 20:13:29 -0500

    One-step function

---
+++ R/one-step.R
@@ -0,0 +1,51 @@
+#' Fits and tidies MRP outputs in one step
+#'
+#' @details Combines `fit_brms`, `poststrat_draws`, and `direct_est`. See `scatter_45`
+#'  for options on visualization
+#'
+#' @param add_on Any area-level data to be merged with the output,
+#'  for example validation data
+#'
+#' @examples
+#' \dontrun{
+#' mrp_fit <- mrp_onestep(response ~ (1|educ) + (1|cd),
+#'                        .data = cces_GA,
+#'                        poststrat_tgt = acs_GA,
+#'                        area_var = "cd",
+#'                        weight_var = "weight_post",
+#'                        add_on = distinct(acs_GA, cd, clinton_vote))
+#'
+#' scatter_45(mrp_fit, clinton_vote, p_mrp_est,
+#'            xlab = "Clinton Vote", ylab = "MRP Estimate")
+#' }
+#'
+#' @export
+mrp_onestep <- function(.formula, .data,
+                        poststrat_tgt,
+                        area_var = "cd",
+                        weight_var,
+                        add_on = NULL,
+                        ...) {
+
+  # direct
+  drct_fit <- direct_ests(.formula, .data, area_var = area_var, weight_var = weight_var)
+
+  # brms
+  brms_fit <- fit_brms(.formula, .data, ...)
+
+  # P-step
+  post_fit <- poststrat_draws(brms_fit, poststrat_tgt, area_var = area_var)
+
+  # summarize
+  post_sum <- summ_sims(post_fit)
+
+  # combine
+  out <- left_join(drct_fit, post_sum, by = area_var)
+
+  # optional
+  if (!is.null(add_on)) {
+    out <- left_join(out, add_on, by = area_var)
+  }
+
+  return(out)
+}
