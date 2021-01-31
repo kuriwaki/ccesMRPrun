68e3b5b8bd69ba4692326cc5c38088f92afc3b18
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-01-30 19:14:23 -0500

    Add direct estimator wrapper

---
+++ R/direct-estimators.R
@@ -0,0 +1,35 @@
+#' Direct Estimator
+#'
+#' Collapses survey data to get direct estimates (i.e. non-pooled sample porportions)
+#'
+#' @param .data Survey data to be collapsed
+#' @param .formula MRP formula. Only thing that will be used is the outcome
+#'  variable (a binary variable)
+#' @param weight_var Character for the variable that corresponds to weights.
+#'
+#' @importFrom Formula as.Formula
+#' @importFrom dplyr group_by summarize `%>%`
+#'
+#' @export
+#'
+#' @examples
+#'  direct_ests(response ~ (1|cd), cces_GA,
+#'              area_var = "cd",
+#'              weight_var = "weight_post")
+#'
+direct_ests <- function(.formula, .data, area_var, weight_var) {
+  Form <- as.Formula(.formula)
+  outcome_var <- all.vars(formula(Form, lhs = 1, rhs = 0))
+
+  .data %>%
+    group_by(!!!syms(area_var)) %>%
+    summarize(p_raw = mean(.data[[outcome_var]],
+                           na.rm = TRUE),
+              p_wt = weighted.mean(.data[[outcome_var]],
+                                    .data[[weight_var]],
+                                    na.rm = TRUE),
+              n_raw = sum(!is.na(.data[[outcome_var]])),
+              .groups = "drop")
+}
+
+
