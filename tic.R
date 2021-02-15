719b54b7047131f46196cfcf1e06a5f04fc8a746
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-15 14:51:04 -0500

    Add patchwork for README only

--- tic.R
+++ tic.R
@@ -4,6 +4,9 @@ do_package_checks(codecov = FALSE)
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
+  get_stage("install") %>%
+    add_step(step_install_cran("patchwork"))
+
   do_readme_rmd()
   do_pkgdown()
 }
