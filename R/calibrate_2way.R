6d70c00fb0cc2d8306b4a8be505bd27902a39ba9
sou412 <sou412snow@gmail.com>
2022-01-12 18:44:16 +0900

    Fix minor typos in 2way calib script

--- R/calibrate_2way.R
+++ R/calibrate_2way.R
@@ -210,16 +210,16 @@ twoway_grad_fn <- function(par, obj) {
   dat$pi_adj_sq <- dat$pi_adj * (1 - dat$pi_adj)
 
   ## fix area and average over groups
-  by_area <- split(dat, obj$ind_area)
-  avg_area <- map_dbl(by_area, function(X) with(X, weighted.mean(pi_adj, n)))
+  by_area   <- split(dat, obj$ind_area)
+  avg_area  <- map_dbl(by_area, function(X) with(X, weighted.mean(pi_adj, n)))
   loss_area <- obj$tgt_area - avg_area
 
   ## fix groups and average over areas
-  by_group <- split(dat, obj$ind_group)
-  avg_group <- map_dbl(by_group, function(X) with(X, weighted.mean(pi_adj, n)))
+  by_group   <- split(dat, obj$ind_group)
+  avg_group  <- map_dbl(by_group, function(X) with(X, weighted.mean(pi_adj, n)))
   loss_group <- obj$tgt_group - avg_group
 
-  ## grad wrt areas -----------------------------
+  ## grad wrt groups -----------------------------
   ## first term
   ### G by J
   Agj_tmp <- map2(by_area, loss_area, function(x, y) {
@@ -230,13 +230,13 @@ twoway_grad_fn <- function(par, obj) {
   Ag_1 <- colSums(do.call(rbind, Agj_tmp))
 
   ## second term
-  Ag_2 <- map_dbl(by_group,
-            function(x) sum(x$n * x$pi_adj_sq) / obj$n_total
-          ) * loss_group
+  Ag_2 <- map_dbl(by_group, function(x) {
+    sum(x$n * x$pi_adj_sq) / obj$n_total
+  }) * loss_group
 
   grad_group <- -2 * (Ag_1 + Ag_2)
 
-  ## grad wrt groups ----------------------------
+  ## grad wrt areas ----------------------------
   ## first term
   Agj_tmp <- map2(by_group, loss_group, function(x, y) {
     as.vector(x$n * x$pi_adj_sq * y) / obj$n_total
@@ -246,10 +246,12 @@ twoway_grad_fn <- function(par, obj) {
   Aj_1 <- colSums(do.call(rbind, Agj_tmp))
 
   ## second term
-  Aj_2 <- map_dbl(by_area,
-            function(x) sum(x$n * x$pi_adj_sq) / obj$n_total
-          ) * loss_area
+  Aj_2 <- map_dbl(by_area, function(x) {
+    sum(x$n * x$pi_adj_sq) / obj$n_total
+  }) * loss_area
   grad_area <- -2 * (Aj_1 + Aj_2)
+
+  ## output gradient
   grad <- c(grad_area, grad_group)
   return(grad)
 }
