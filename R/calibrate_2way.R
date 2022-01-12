9aa85b135befa7a96655c44ae8a8c355224cf886
sou412 <sou412snow@gmail.com>
2022-01-12 18:38:32 +0900

    Consolidate two scripts for 2way calibration

--- R/calibrate_2way.R
+++ R/calibrate_2way.R
@@ -1,39 +1,3 @@
-#' Two-way Calibration objective function
-#' @param par The vector of parameters.
-#' @param obj A list of inputs See `calib_twoway`.
-#'
-#' @importFrom purrr map_dbl
-#' @keywords internal
-twoway_obj_fn <- function(par, obj) {
-  dat <- obj$dat
-
-  ## adjustment factor
-  delta <- obj$X %*% par
-
-  ## adjusted value
-  dat$pi_adj <- invlogit(dat$pi_logit + delta)
-
-  ## objective wrt district ---------------------------------
-  # dat %>%
-  #   group_by(!!sym(obj$var_area)) %>%
-  #   summarise(pi_group = sum(n * pi_adj) / sum(n))
-  by_area <- split(dat, obj$ind_area)
-  avg_area <- map_dbl(by_area, function(X) with(X, weighted.mean(pi_adj, n)))
-  loss_area <- sum((obj$n_j / obj$n_total) * (obj$tgt_area - avg_area)^2)
-
-
-  ## objective wrt racial groups ----------------------------
-  by_group <- split(dat, obj$ind_group)
-  avg_group <- map_dbl(by_group, function(X) with(X, weighted.mean(pi_adj, n)))
-  loss_group <- sum((obj$n_g / obj$n_total) * (obj$tgt_group - avg_group)^2)
-
-
-  ## sum of two losses
-  loss <- loss_area + loss_group
-
-  return(loss)
-}
-
 #' Find two-way correction for cell estimates
 #'
 #'
@@ -190,4 +154,102 @@ calib_twoway <- function(
   data$est_corrected <- invlogit(logit_ghitza(data$est) + delta)
   data$delta <- delta
   return(data)
+}
+
+#' Two-way Calibration objective function
+#' @param par The vector of parameters.
+#' @param obj A list of inputs See `calib_twoway`.
+#'
+#' @importFrom purrr map_dbl
+#' @keywords internal
+twoway_obj_fn <- function(par, obj) {
+  dat <- obj$dat
+
+  ## adjustment factor
+  delta <- obj$X %*% par
+
+  ## adjusted value
+  dat$pi_adj <- invlogit(dat$pi_logit + delta)
+
+  ## objective wrt district ---------------------------------
+  # dat %>%
+  #   group_by(!!sym(obj$var_area)) %>%
+  #   summarise(pi_group = sum(n * pi_adj) / sum(n))
+  by_area <- split(dat, obj$ind_area)
+  avg_area <- map_dbl(by_area, function(X) with(X, weighted.mean(pi_adj, n)))
+  loss_area <- sum((obj$n_j / obj$n_total) * (obj$tgt_area - avg_area)^2)
+
+
+  ## objective wrt racial groups ----------------------------
+  by_group <- split(dat, obj$ind_group)
+  avg_group <- map_dbl(by_group, function(X) with(X, weighted.mean(pi_adj, n)))
+  loss_group <- sum((obj$n_g / obj$n_total) * (obj$tgt_group - avg_group)^2)
+
+
+  ## sum of two losses
+  loss <- loss_area + loss_group
+
+  return(loss)
+}
+
+#' Two-way Calibration gradient function
+#'
+#' @param par A vector of parameters.
+#' @param obj A list of inputs, see `calib_twoway`
+#' @importFrom purrr map_dbl map2
+#' @keywords internal
+twoway_grad_fn <- function(par, obj) {
+  dat <- obj$dat
+
+  ## note
+  ## - par is a stacked vector par = [delta(area), delta(group)]
+
+  ## adjustment factor
+  delta <- obj$X %*% par
+  dat$pi_adj <- invlogit(dat$pi_logit + delta)
+  dat$pi_adj_sq <- dat$pi_adj * (1 - dat$pi_adj)
+
+  ## fix area and average over groups
+  by_area <- split(dat, obj$ind_area)
+  avg_area <- map_dbl(by_area, function(X) with(X, weighted.mean(pi_adj, n)))
+  loss_area <- obj$tgt_area - avg_area
+
+  ## fix groups and average over areas
+  by_group <- split(dat, obj$ind_group)
+  avg_group <- map_dbl(by_group, function(X) with(X, weighted.mean(pi_adj, n)))
+  loss_group <- obj$tgt_group - avg_group
+
+  ## grad wrt areas -----------------------------
+  ## first term
+  ### G by J
+  Agj_tmp <- map2(by_area, loss_area, function(x, y) {
+    as.vector(x$n * x$pi_adj_sq * y) / obj$n_total
+  })
+
+  ### sum over j
+  Ag_1 <- colSums(do.call(rbind, Agj_tmp))
+
+  ## second term
+  Ag_2 <- map_dbl(by_group,
+            function(x) sum(x$n * x$pi_adj_sq) / obj$n_total
+          ) * loss_group
+
+  grad_group <- -2 * (Ag_1 + Ag_2)
+
+  ## grad wrt groups ----------------------------
+  ## first term
+  Agj_tmp <- map2(by_group, loss_group, function(x, y) {
+    as.vector(x$n * x$pi_adj_sq * y) / obj$n_total
+  })
+
+  ## sum over group
+  Aj_1 <- colSums(do.call(rbind, Agj_tmp))
+
+  ## second term
+  Aj_2 <- map_dbl(by_area,
+            function(x) sum(x$n * x$pi_adj_sq) / obj$n_total
+          ) * loss_area
+  grad_area <- -2 * (Aj_1 + Aj_2)
+  grad <- c(grad_area, grad_group)
+  return(grad)
 }
